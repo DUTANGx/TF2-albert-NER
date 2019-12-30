7ac71eb0c4e8b68cd4d4966a9973a7f1d6a89f8a
du <tangdu@astri.org>
2019-12-30 17:49:37 +0800

    added boson ner api

--- tf2service.py
+++ tf2service.py
@@ -6,17 +6,89 @@ from vocab import tokenizer
 from utils import to_comma_seperated_array
 
 
+def ner_boson(sentence, max_len=256):
+    processed_sentence = tokenizer.tokenize_to_ids(sentence, max_len)
+    req = '{"inputs": %s}' % to_comma_seperated_array(
+        np.array(processed_sentence))
+    response = json.loads(requests.post(
+        url='http://0.0.0.0:8501/v1/models/albert_ner/versions/2:predict',
+        data=req).text)['outputs']
+    preds = np.array(response[0])
+    transition_params = np.load(
+        'models/albert_crf/saved_model/2/transition.npy')
+    decoded = tfa.text.viterbi_decode(preds, transition_params)[0]
+    print(decoded)
+    real_len = len(sentence)
+    entity = {}
+    entities = []
+
+    def check_entity_over(x, pos):
+        if x:
+            x['end_position'] = pos - 1
+            entities.append(dict(x))
+            x.clear()
+        else:
+            pass
+
+    # to prevent mis labelling
+    def check_type_and_add(x, type_name):
+        if x and x['type'] == type_name:
+            x['str'] += (sentence[i])
+
+    for i in range(0, real_len):
+        if decoded[i + 1] == 1:
+            check_entity_over(entity, i)
+            entity = {'type': 'person', 'start_position': i,
+                      'str': sentence[i]}
+        elif decoded[i + 1] == 2:
+            check_type_and_add(entity, 'person')
+        elif decoded[i + 1] == 3:
+            check_entity_over(entity, i)
+            entity = {'type': 'location', 'start_position': i,
+                      'str': sentence[i]}
+        elif decoded[i + 1] == 4:
+            check_type_and_add(entity, 'location')
+        elif decoded[i + 1] == 5:
+            check_entity_over(entity, i)
+            entity = {'type': 'organization', 'start_position': i,
+                      'str': sentence[i]}
+        elif decoded[i + 1] == 6:
+            check_type_and_add(entity, 'organization')
+        elif decoded[i + 1] == 7:
+            check_entity_over(entity, i)
+            entity = {'type': 'time', 'start_position': i,
+                      'str': sentence[i]}
+        elif decoded[i + 1] == 8:
+            check_type_and_add(entity, 'time')
+        elif decoded[i + 1] == 9:
+            check_entity_over(entity, i)
+            entity = {'type': 'company_name', 'start_position': i,
+                      'str': sentence[i]}
+        elif decoded[i + 1] == 10:
+            check_type_and_add(entity, 'company_name')
+        elif decoded[i + 1] == 11:
+            check_entity_over(entity, i)
+            entity = {'type': 'product_name', 'start_position': i,
+                      'str': sentence[i]}
+        elif decoded[i + 1] == 12:
+            check_type_and_add(entity, 'product_name')
+        else:
+            check_entity_over(entity, i)
+
+    return entities
+
+
 def ner(sentence, max_len=128):
     processed_sentence = tokenizer.tokenize_to_ids(sentence, max_len)
     req = '{"inputs": %s}' % to_comma_seperated_array(
         np.array(processed_sentence))
     response = json.loads(requests.post(
-        url='http://0.0.0.0:8501/v1/models/albert_ner:predict',
+        url='http://0.0.0.0:8501/v1/models/albert_ner/versions/1:predict',
         data=req).text)['outputs']
     preds = np.array(response[0])
-    transition_params = np.load('models/albert_crf/saved_model/1/transition.npy')
+    transition_params = np.load(
+        'models/albert_crf/saved_model/1/transition.npy')
     decoded = tfa.text.viterbi_decode(preds, transition_params)[0]
-    print(decoded)
     real_len = len(sentence)
     entity = {}
     entities = []
@@ -34,23 +106,23 @@ def ner(sentence, max_len=128):
             x['str'] += (sentence[i])
 
     for i in range(0, real_len):
-        if decoded[i+1] == 1:
+        if decoded[i + 1] == 1:
             check_entity_over(entity, i)
             entity = {'type': 'person', 'start_position': i,
                       'str': sentence[i]}
-        elif decoded[i+1] == 2:
+        elif decoded[i + 1] == 2:
             check_type_and_add(entity, 'person')
-        elif decoded[i+1] == 3:
+        elif decoded[i + 1] == 3:
             check_entity_over(entity, i)
             entity = {'type': 'location', 'start_position': i,
                       'str': sentence[i]}
-        elif decoded[i+1] == 4:
+        elif decoded[i + 1] == 4:
             check_type_and_add(entity, 'location')
-        elif decoded[i+1] == 5:
+        elif decoded[i + 1] == 5:
             check_entity_over(entity, i)
             entity = {'type': 'organization', 'start_position': i,
                       'str': sentence[i]}
-        elif decoded[i+1] == 6:
+        elif decoded[i + 1] == 6:
             check_type_and_add(entity, 'organization')
         else:
             check_entity_over(entity, i)
@@ -61,4 +133,6 @@ def ner(sentence, max_len=128):
 if __name__ == '__main__':
     sentence = '马云，祖籍浙江省绍兴嵊州市谷来镇，后父母移居杭州，出生于浙江省杭州市，' \
                '中国企业家，中国共产党党员。曾为亚洲首富、阿里巴巴集团董事局主席。'
+    # sentence2 = '2000年在深圳，马化腾作为腾讯的总经理参与了微信的开发。'
     print(ner(sentence))
+    print(ner_boson(sentence))
